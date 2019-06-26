7e9ec4b211e6e1061fbcaf73ca39c3de261700d4
panachai <pananer02@hotmail.com>
2019-06-26 09:29:11 +0700

    อัพโหลด C++

---
+++ lab3/lab3/lab3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout << "Student Code : " << " 6206021610021" << endl;
+	cout << "Student Name : " << " Panachai Techaphaisalkul" << endl;
+	cout << "Division : "<< "Information Techanology"<<endl;
+	cout << "Year : "<< "1" << endl;
+	cout << "Room : "<< "RA" << endl;
+	cout << "Sex : "<< "Male" << endl;
+	cout << "Age : "<< "20" << endl;
+	cout << endl;
+	cout << "    #    "<<endl;
+	cout << "   # #   "<<endl;
+	cout << "  #   #  "<<endl;
+	cout << " #     # "<<endl;
+	cout << " ####### "<<endl;
+	cout << " #     # "<<endl;
+	return 0 ;
+}
